6dd26d43de75238e6b56066dac0390bf36deff59
Th4Dude <gallottimaurizio@yahoo.it>
2023-02-27 13:23:25 +0200

    non completo 2

---
+++ snack-3/main.js
@@ -0,0 +1,15 @@
+'use strict'
+
+
+const nomi = ['maurizio','tonino','matteo'];
+const cognomi = ['gallotti','iacone','sasso'];
+
+console.log(nomi)
+console.log(cognomi)
+
+/* let gatsby = [nomi + cognomi]
+console.log(gatsby) */
+
+const gatsby = []
+
+for(let i = 0 ; i < 3; i++ )
