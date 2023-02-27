1b2bb94f1a715d4f4ae5eae432a323b6a0fc8b37
Th4Dude <gallottimaurizio@yahoo.it>
2023-02-27 12:57:59 +0200

    eccco

---
+++ snack-2/main.js
@@ -0,0 +1,20 @@
+'use strict'
+
+
+
+/* JSsnack2
+Inserisci un numero, se è pari stampa il numero,
+se è dispari stampa il numero successivo */
+
+
+
+
+//check if the number is even
+if(number % 2 == 0) {
+    console.log("numero è pari");
+}
+
+// if the number is odd
+else {
+    console.log("numero è dispari");
+}
